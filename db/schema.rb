a8e3bd76d07c6e29cceecf5e25620c885aaa76bb
VincentLin0619 <vincentlin19910619@gmail.com>
2022-09-29 15:49:33 +0800

    add carrierwave add ImagesColumnToProducts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_09_28_095704) do
+ActiveRecord::Schema.define(version: 2022_09_29_074319) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 2022_09_28_095704) do
     t.integer "sku", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "images"
   end
 
   create_table "users", force: :cascade do |t|
