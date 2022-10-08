90b9d7da71bee246402c316802b676fd265473d3
VincentLin0619 <vincentlin19910619@gmail.com>
2022-10-08 14:40:45 +0800

    fix migration

--- db/migrate/20221008062953_create_posts.rb
+++ db/migrate/20221008062953_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[6.1]
   def change
     create_table :posts do |t|
       t.string :title
-      t.text :content
+      t.text :content, null: true
 
       t.timestamps
     end
