e4e269f1cb9da193f5e5510954afeb38306df886
Vincentlin0619 <vincentlin19910619@gmail.com>
2022-11-16 10:16:33 +0800

    註解掉mail設定的domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,14 +7,14 @@ Rails.application.configure do
   config.cache_classes = true
   host = "https://vintdemoapp.tk"
   config.assets.css_compressor = nil
-  config.action_mailer.default_url_options = { host: host }
+  # config.action_mailer.default_url_options = { host: host }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     port: 587,
     address: "smtp.mailgun.org",
     user_name: ENV["MAILGUN_USERNAME"],
     password: ENV["MAILGUN_PASSWORD"],
-    domain: host,
+    # domain: host,
     authentication: :plain,
   }
 
